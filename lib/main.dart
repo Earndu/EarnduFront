2e4760d19656040ffc67abbf8b91dd8623236a5d
orlando <59018852+ingkoon@users.noreply.github.com>
2021-02-18 21:59:24 +0900

    json 파싱

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'src/Earndu.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(
--- lib/src/Earndu.dart
+++ lib/src/Earndu.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // 만약 요청이 실패하면, 에러를 던집니다.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  // factory 생성자. Post 타입의 인스턴스를 반환
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title'],
+        body: json['body']);
+  }
+}
+
+// 여기서 부터 시작
 class Earndu extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,30 +64,46 @@ class Earndu extends StatelessWidget {
 }
 
 class App extends StatefulWidget {
+  App({Key key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return AppState(); // 전 단계에서 만든 클래스
-  }
+  _AppState createState() => _AppState();
+  // 전 단계에서 만든 클래스
 }
 
-class AppState extends State<App> {
-  int counter = 0;
+class _AppState extends State<App> {
+  Future<Post> post;
+
+  @override
+  void initState() {
+    super.initState();
+    post = fetchPost();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'tt',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
       home: Scaffold(
-        body: Text('$counter'),
         appBar: AppBar(
-          title: Text("이거또한 임시로 만들어 본거심을 명시하는 바임미다."),
+          title: Text("Json 읽어보기용 위젯"),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            //이벤트 콜백 함수
-            setState(() {
-              counter += 1;
-            });
-          },
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.body);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
