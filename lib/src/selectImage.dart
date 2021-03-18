4785bc6de121c592d6c9c2fc50b6f6e330299d22
Orlando <59018852+ingkoon@users.noreply.github.com>
2021-03-18 23:11:32 +0900

    Update selectImage.dart

--- lib/src/selectImage.dart
+++ lib/src/selectImage.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class selectImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      //디버깅 텍 제거
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
       home: Scaffold(
         body: Center(
+          //컨텐츠들의 전체적인 구성은 column타입으로 지정할 것
           child: Column(
             children: <Widget>[
+              //이미지 선택 관련 텍스트 출력을 위한 컨테이너
               Container(
+                //형식에 맞게 마진 지정
                 margin: const EdgeInsets.only(top: 54, bottom: 38),
                 child: Text("choose your character",
                     style: const TextStyle(
@@ -19,6 +24,7 @@ class selectImage extends StatelessWidget {
                         fontSize: 25.0),
                     textAlign: TextAlign.left),
               ),
+              //코끼리 이미지가 들어갈 컨테이너
               Container(
                 height: 180,
                 width: 180,
@@ -44,6 +50,7 @@ class selectImage extends StatelessWidget {
                   },
                 ),
               ),
+              //이후의 컨테이너들은 전부 row타입으로 변경하여 내부 요소로 삽입
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -162,6 +169,7 @@ class selectImage extends StatelessWidget {
   }
 }
 
+//이미지 선택 관련 함수
 Widget chooseImage(BuildContext context, String imageName) {
   return Container(
     height: 150,
